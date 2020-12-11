8250938124d93ccd104dd73abd73925eb4e324bd
Benjamin Krause <benjamin.f.krause@uia.no>
2020-12-12 00:54:13 +0100

    Update incorrect files

--- hass-backup/run.sh
+++ hass-backup/run.sh
@@ -12,21 +12,21 @@ USERNAME=$(jq --raw-output ".username" $CONFIG_PATH)
 PASSWORD=$(jq --raw-output ".password" $CONFIG_PATH)
 
 # Make sure all variables are configured
-if [ -z "$HOST" ] || [ -z "$SSL" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]
+if [ -z "$HOST" ] || [ -z "$PORT" ] || [ -z "$SSL" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]
 then
     echo "Please configure the addon before use"
     exit 1
 fi
 
 # Create the backup
-echo "Creating backup: \"$(date +'%d.%m.%Y %H:%M')\""
+echo "Creating backup ($(date +'%d.%m.%Y %H:%M'))"
 slug=$(${HA} snapshots new --name="${name}" | cut -d' ' -f2)
 echo "Backup created: ${slug}"
 
 # Upload backup
 if ${SSL} == true; then HTTP="https"; else HTTP="http"; fi
 upload_url="${HTTP}://${HOST}:${PORT}/upload"
-echo "Uploading ${slug}.tar to ${remote_url}"
+echo "Uploading ${slug}.tar to ${upload_url}"
 curl -u ${USERNAME}:${PASSWORD} -X POST -m 1800 -s -F file=@"/backup/${slug}.tar" ${upload_url}
 
 # Delete local backup
